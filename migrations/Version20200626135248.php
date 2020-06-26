5a5b3d3641f1b84723241640eb1c5d06111a28de
Thomas Sihapanya <contact@tomsihap.fr>
2020-06-26 15:53:24 +0200

    :card_file_box: Create migration (Student, Rating tables + relation)

---
+++ migrations/Version20200626135248.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200626135248 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE rating (id INT AUTO_INCREMENT NOT NULL, student_id INT NOT NULL, value INT NOT NULL, subject VARCHAR(100) NOT NULL, INDEX IDX_D8892622CB944F1A (student_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE student (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(150) NOT NULL, lastname VARCHAR(150) NOT NULL, birthdate DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE rating ADD CONSTRAINT FK_D8892622CB944F1A FOREIGN KEY (student_id) REFERENCES student (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE rating DROP FOREIGN KEY FK_D8892622CB944F1A');
+        $this->addSql('DROP TABLE rating');
+        $this->addSql('DROP TABLE student');
+    }
+}
